49ca8c1a494d3a42b1d202a6c24dedab616ba2b2
Alba Marco <134753598+alba-muma@users.noreply.github.com>
2025-05-05 22:28:29 +0200

    Add files via upload

--- blueprints/routes.py
+++ blueprints/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, Response
 from telegram.conectar_telegram import obtener_info_anuncio
+from telegram.storage import obtener_mensajes
+import json
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/ultimo-anuncio', methods=['GET'])
@@ -9,3 +11,9 @@ def get_ultimo_anuncio():
     if info is None:
         return jsonify({"error": "No hay anuncios procesados"}), 204 
     return jsonify(info), 200
+
+@main_bp.route('/api/mensajes', methods=['GET'])
+def get_mensajes():
+    mensajes = obtener_mensajes()
+    response = Response(json.dumps(mensajes, ensure_ascii=False), content_type="application/json; charset=utf-8")
+    return response
